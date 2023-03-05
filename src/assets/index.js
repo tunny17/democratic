f312fb2c0b1819d5164758ec1b163fe19c41f80d
tunny17 <tvnji17@gmail.com>
2023-03-06 00:05:08 +0100

    - completion of the footer section - addition of animation tweaks

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,3 +8,4 @@ export { default as slide1 } from './slide1.png';
 export { default as slide2 } from './slide2.png';
 export { default as slide3 } from './slide3.png';
 export { default as slide4 } from './slide4.png';
+export { default as social } from './social.svg';
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { globe, logo, social } from '../../assets'
 import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div  className='footer-container'>
+      <div className="logo-container">
+        <img src={logo} alt="a logo" />
+        <h2>Democratic</h2>
+      </div>
+      <div>
+        <p>Ilorin Innovation Hub</p>
+        <p>Kwara State Government</p>
+        <p>Move Co.</p>
+      </div>
+      <div>
+        <img src={social} alt="" />
+      </div>
+      <div>
+        <p>Democratic is a web-based app designed to monitor elections in real time and display automatically compiled results at all levels. It also provides up-to-date data such as registered voter counts for each LGA/Wards/PU, expected voter turnout, winning criteria, and distribution of political strongholds. </p>
+      </div>
+    </div>
   )
 }
 
